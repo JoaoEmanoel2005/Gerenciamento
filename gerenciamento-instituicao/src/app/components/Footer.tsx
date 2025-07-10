cf3a14a740570b66b84e7846545e0f6e4e798511
João Emanoel <jeas9999@gmail.com>
2025-07-10 20:22:53 -0300

    inicio do projeto full stack com api

--- gerenciamento-instituicao/src/app/components/Footer.tsx
+++ gerenciamento-instituicao/src/app/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-800 text-gray-300 py-6 mt-12">
-        <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
-          <p>© {new Date().getFullYear()} Treino Next. Todos os direitos reservados.</p>
+<div className="max-w-7xl mx-auto px-6 flex flex-col justify-between items-center">
+<p>© {new Date().getFullYear()} Projeto de treino em Next. Todos os direitos reservados a João Emanoel, criador deste projeto.</p>
           <div className="flex gap-4 mt-4 md:mt-0">
             <a href="https://github.com/JoaoEmanoel2005" target="_blank" rel="noopener noreferrer" className="hover:text-white">
               GitHub
---
+++ gerenciamento-instituicao/src/pages/api/hello.js
@@ -0,0 +1,5 @@
+
+export default function handler(req, res) {
+    res.status(200).json({ mensagem: 'API do Next funcionando!' });
+  }
+  
