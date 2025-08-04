00ac1af3ff87d87b6949cbed920e07ebf25a6369
João Emanoel <jeas9999@gmail.com>
2025-08-03 22:08:00 -0300

    Estilizando a div do formulario de login

--- gerenciamento-instituicao/src/app/page.tsx
+++ gerenciamento-instituicao/src/app/page.tsx
@@ -1,36 +1,34 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex item-center justify-center bg-gray-100">
+    <main className="min-h-screen flex items-center justify-center bg-white">
 
-      <div className="w-full max-w-sm bg-withe p-8 rounded-lg shadow-md border-1">
+      <div className="w-full max-w-xs h-auto sm:h-[400px] bg-gray-100 p-6 rounded-xl shadow-lg flex flex-col justify-center border-1">
 
-        <h1 className="text-2x1 font-bold text-center mb-6">Login</h1>
+        <h1 className="text-3xl font-bold text-center mb-10 border-b border=gray-400 pb-2">Login</h1>
 
         <form className="flex flex-col gap-4">
 
           <input
             type="email"
             placeholder="Email"
-            className="p-3 border nporder-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="p-3 border border-black rounded focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-700"
           />
 
           <input
             type="password"
-            placeholder="Password"
-            className="p-3 border nporder-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Senha"
+            className="p-3 border border-black rounded focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-gray-700"
           />
 
           <button
             type="submit"
-            className="bg-blue-600 text-white p-3 rounded hover:bg-blue-600 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            className="bg-blue-600 text-white py-3 rounded hover:bg-blue-700 transition"
           >
-            Enter
+            Entrar
           </button>
+
         </form>
       </div>
     </main>
-
-
-
   );
 }
