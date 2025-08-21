a3a43620a58967a0f7b30ab6e48f8c4df3f42553
João Emanoel <jeas9999@gmail.com>
2025-08-20 21:30:35 -0300

    atualizacao do local do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Projeto Next.js",
+  title: "Projeto estudo Next.js",
   description: "Projeto de treino com Next.js, React e Tailwind CSS, com foco em boas práticas de desenvolvimento. Focando em aprimorar habilidades em Next.js, React e Tailwind CSS.",
 };
 
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className="bg-gray-50 text-gray-900">
         {/* NAVBAR FIXA */}
         <nav className="bg-blue-600 text-white px-6 py-4 flex gap-6">
-          <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/home" className="hover:underline">Inicio</Link>
           <Link href="/sobre" className="hover:underline">Sobre</Link>
           <Link href="/contato" className="hover:underline">Contato</Link>
         </nav>
